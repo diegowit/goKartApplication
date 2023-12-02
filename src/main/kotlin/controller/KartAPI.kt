f05e7261b8180a3805d612345305ce28b958ffbf
diego <20098757@mail.wit.ie>
2023-12-02 22:49:57 +0000

    delete function implemented in KartAPI

--- src/main/kotlin/controller/KartAPI.kt
+++ src/main/kotlin/controller/KartAPI.kt
@@ -2,6 +2,7 @@ package controller
 import models.Lap
 import models.Kart
 import persistence.Serializer
+import utils.Utilities.isValidListIndex
 import java.util.ArrayList
 
 class KartAPI(serializerType: Serializer) {
@@ -13,31 +14,35 @@ class KartAPI(serializerType: Serializer) {
     private fun getId() = lastId++
 
 
+    fun isValidIndex(index: Int): Boolean {
+        return isValidListIndex(index, karts)
+    }
+
+    fun findKart(index: Int): Kart? {
+        return if (isValidIndex(index)) {
+            karts[index]
+        } else null
+    }
+
+
     fun add(kart: Kart): Boolean {
         kart.KartId = getId()
         return karts.add(kart)
     }
 
-    fun delete(id: Int) = karts.removeIf{kart -> kart.KartId == id}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fun deleteKart(indexToDelete: Int): Kart? {
+        return if (isValidListIndex(indexToDelete, karts)) {
+            karts.removeAt(indexToDelete)
+        } else null
+    }
 
 }
+
+
+
+
+
+
+
+
+
