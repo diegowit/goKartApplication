5cdd8058a2205e9e5be1acdc1c8f98b571b67eed
diego <20098757@mail.wit.ie>
2023-12-02 21:36:01 +0000

    add kart function implemented in KartAPI

--- src/main/kotlin/controller/KartAPI.kt
+++ src/main/kotlin/controller/KartAPI.kt
@@ -1,5 +1,29 @@
 package controller
+import models.Lap
+import models.Kart
+import persistence.Serializer
+import java.util.ArrayList
 
-class KartAPI {
+class KartAPI(serializerType: Serializer) {
+    private var serializer: Serializer = serializerType
+
+    private var karts = ArrayList<Kart>()
+
+    fun add(kart: Kart): Boolean {
+        return karts.add(kart)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
