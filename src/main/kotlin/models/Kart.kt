f4aff04be565f7a2f036c9cdf4d19e0e21f088d4
diego <20098757@mail.wit.ie>
2023-12-03 00:41:57 +0000

    Methods added for laps

--- src/main/kotlin/models/Kart.kt
+++ src/main/kotlin/models/Kart.kt
@@ -1,5 +1,5 @@
 package models
-
+import utils.Utilities
 data class Kart(
 
     var KartId: Int = 0,
@@ -10,7 +10,62 @@ data class Kart(
     var isElectric: Boolean,
     var laps: MutableSet<Lap> = mutableSetOf()
 
-)
+) {
+    private var lastLap = 0
+    private fun getLapId() = lastLap++
+
+    fun addLap(lap: Lap): Boolean {
+        lap.lapId = getLapId()
+        return laps.add(lap)
+    }
+
+    fun deleteLap(id: Int): Boolean {
+        return laps.removeIf { lap -> lap.lapId == id }
+    }
+  fun numberOfLaps() = laps.size
+
+    fun findOne(id: Int): Lap?{
+        return laps.find { lap -> lap.lapId == id  }
+    }
+    fun updateLap(id: Int, newLap: Lap): Boolean {
+        val foundLap = findOne(id)
+
+        if (foundLap != null) {
+            foundLap.driverName = newLap.driverName
+            foundLap.driverAge = newLap.driverAge
+            foundLap.distance = newLap.distance
+            foundLap.time = newLap.time
+            foundLap.speed = newLap.speed
+            foundLap.isLapCompleted = newLap.isLapCompleted
+            return true
+        }
+
+        //if the object was not found, return false, indicating that the update was not successful
+        return false
+    }
+
+    fun checkLapCompletionStatus(): Boolean {
+        if (laps.isNotEmpty()) {
+            for (lap in laps) {
+                if (!lap.isLapCompleted) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+fun listLaps() =
+    if (laps.isEmpty())  "\tNO LAPS ADDED"
+    else  Utilities.formatSetString(laps)
+
+
+
+
+
+
+
+}
 
 
 
