ff856169bdaae0c6642d7bc48b54805a0a36e764
diego <20098757@mail.wit.ie>
2023-12-03 15:11:20 +0000

    Methods and Menu added in the Main.kt

---
+++ src/test/kotlin/utils/UtilitiesTest.kt
@@ -0,0 +1,20 @@
+package utils
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import controller.KartAPI
+import models.Kart
+import models.Lap
+
+class UtilitiesTest {
+
+    @Test
+    fun validRangeWorksWithPositiveTestData() {
+        Assertions.assertTrue(Utilities.validRange(5, 0, 10))
+    }
+
+    @Test
+    fun validRangeWorksWithNegativeTestData() {
+        Assertions.assertFalse(Utilities.validRange(15, 0, 10))
+    }
+}
